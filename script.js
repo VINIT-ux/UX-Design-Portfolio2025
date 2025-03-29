3dac2fb5b0184efb387cedcb7e37e87e37ac6095
Vinit-ux <mnvinit590@gmail.com>
2025-03-29 20:01:49 +0530

    CV Update 2.1

--- script.js
+++ script.js
@@ -48,7 +48,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const downloadLink = document.createElement('a');
         
         // Set the file path and filename
-        downloadLink.href = 'cv/M N VINIT_Product Designer CV 2025'; // Correct the folder name
+        downloadLink.href = 'cv/M N VINIT_Product Designer CV 2025.pdf';
         downloadLink.download = 'M N VINIT_Product Designer.pdf'; // Set the desired file name
         
         // Append the anchor to the body, click it to start the download, and remove it
@@ -67,8 +67,8 @@ document.addEventListener('DOMContentLoaded', () => {
             // Create a temporary anchor element for the download
             const downloadLink = document.createElement('a');
             
-            downloadLink.href = 'cv/M N VINIT_CV.pdf'; // Correct the folder name
-            downloadLink.download = 'M N VINIT_CV.pdf'; // Set the desired file name
+            downloadLink.href = 'cv/M N VINIT_Product Designer CV 2025.pdf'; // Correct the folder name
+            downloadLink.download = 'M N VINIT_Product Designer.pdf'; // Set the desired file name
             
             // Append the anchor to the body, click it to start the download, and remove it
             document.body.appendChild(downloadLink);
